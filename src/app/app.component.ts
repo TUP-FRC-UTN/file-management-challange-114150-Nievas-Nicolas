f3f041993cbf2b8279ad283b5341d25b405305f6
Nicolas Nievas <114150@tecnicatura.frc.utn.edu.ar>
2024-09-07 19:16:21 -0300

    listado completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FileItem } from '../models/file.item.model';
-import { FILE_LIST } from '../data/file.storage';
+import { FileItem, FileOwner, FileType } from '../models/file.item.model';
+import { FILE_LIST, OWNERS } from '../data/file.storage';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  files: FileItem[] = FILE_LIST;
+  files: (FileItem & { selected: boolean})[] = FILE_LIST.sort((a, b) => {
+    if (a.type !== b.type) {
+      return a.type === FileType.FOLDER ? -1 : 1;
+    }
+    return a.name.localeCompare(b.name);
+  }).map(file => ({ ...file, selected: false }));
   title = 'file-management';
+  owner: FileOwner[] = OWNERS
+
+  deleteSelectedFiles() {
+    this.files = this.files.filter(file => !file.selected);
+  }
+
+  viewForm : boolean = false;
+
+  showForm() {
+    this.viewForm = true;
+  }
 }
