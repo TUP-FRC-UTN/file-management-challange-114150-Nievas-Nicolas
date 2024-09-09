ea5fd4ec7a3b5b7aa177e7b19bacc48d47b82933
Nicolas Nievas <114150@tecnicatura.frc.utn.edu.ar>
2024-09-09 00:27:06 -0300

    formulario terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,14 +22,46 @@ export class AppComponent {
   }).map(file => ({ ...file, selected: false }));
   title = 'file-management';
   owner: FileOwner[] = OWNERS
+  viewForm : boolean = false;
 
   deleteSelectedFiles() {
-    this.files = this.files.filter(file => !file.selected);
-  }
+    const selectedFiles = this.files.filter(file => file.selected);
 
-  viewForm : boolean = false;
+    if(selectedFiles.length === 0){
+      alert('Please select at least one file to delete');
+      console.log('No file selected');
+      return;
+    }
+
+    if(selectedFiles.length > 1 ){
+      const confirmDelete = confirm(`You are about to delete ${selectedFiles.length} files/folders. Are you sure?`);
+
+      if(confirmDelete){
+        this.files = this.files.filter(file => !file.selected);
+        console.log('Files deleted', selectedFiles);
+      }
+    } else{
+      this.files = this.files.filter(file => !file.selected);
+      console.log('File deleted', selectedFiles);
+    }
+    
+  }
 
   showForm() {
     this.viewForm = true;
+  }
+
+  hideForm() {
+    this.viewForm = false;
+  }
+
+  pushForm(file: FileItem) {
+    this.files.push({ ...file, selected: false });
+    this.files.sort((a, b) => {
+      if (a.type !== b.type) {
+        return a.type === FileType.FOLDER ? -1 : 1;
+      }
+      return a.name.localeCompare(b.name);
+    });
   }
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { FileItem, FileType } from '../../models/file.item.model';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  @Output() closeForm = new EventEmitter<void>();
+  @Output() formEmit = new EventEmitter<FileItem>();
+
+  fileType = FileType;
+  fileItem: FileItem = {
+    id: '',
+    name: '',
+    creation: new Date(),
+    owners: [{ name: '', avatarUrl: '' }],
+    type: FileType.FILE,
+    parentId: undefined,
+  };
+
+  sendForm(form: NgForm) {
+    if (form.valid) {
+      this.fileItem.id = Math.random().toString(36).substr(2, 9);
+      this.fileItem.owners = this.fileItem.owners.filter(owner => owner.name.trim() !== '' && owner.avatarUrl.trim() !== '');
+      this.formEmit.emit(this.fileItem);
+      this.closeForm.emit();
+      console.log('Form sent', this.fileItem);
+    }
+    else{
+      alert('Please fill all the fields');
+      console.log('Form not sent', this.fileItem);
+    }
+  }
+
+  cancel() {
+    this.closeForm.emit();
+  }
 
 }
